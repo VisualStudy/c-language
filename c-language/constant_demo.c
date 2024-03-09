8f406a09eddc377fd928dfb24954a814e2721ef9
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-09 11:03:53 +0000

    constant_demo

---
+++ c-language/constant_demo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    const int MAX = 100;
+    const double PI = 3.14;
+    const char GRADE = 'A';
+
+    printf("%d, %.6lf, %c\n", MAX, PI, GRADE);
+
+    return 0;
+}
