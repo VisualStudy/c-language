f935af945ff0c5f9511b66dc71d5c6b9de030aff
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-11 11:07:17 +0000

    character demo

---
+++ c-language/character_demo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char grade = 'A';
+    char num = -128;
+    unsigned char age = 255;
+
+    printf("%c\n", grade);
+    printf("%d\n", num);
+    printf("%d\n", age);
+
+    return 0;
+}
