242042166c50ed141baf1b337e757bf23614863e
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-14 12:16:45 +0000

    input two integer

---
+++ c-language/input_two_integer_.c
@@ -0,0 +1,13 @@
+// input_two_integer.c
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int one, two, _;
+    
+    printf("input two number: _\b");
+    _ = scanf("%d %d", &one, &two);
+    printf("%d %d\n", one, two);
+    return 0;
+}
