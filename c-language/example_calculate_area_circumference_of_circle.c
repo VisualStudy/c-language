b66c4f09698891eef83d1ef633794c8ddb00ce7d
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-16 14:05:03 +0000

    refactoring

--- c-language/example_calculate_area_circumference_of_circle.c
+++ c-language/example_calculate_area_circumference_of_circle.c
@@ -1,6 +1,7 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #define PI 3.141592654
+
 int main(void)
 {
     float r;
@@ -9,7 +10,7 @@ int main(void)
     printf("반지름: ");
     _ = scanf("%f", &r);
 
-    printf("원의 넓이: %f\n", ((r * r) * PI));
+    printf("원의 넓이: %f\n", (PI * (r * r)));
     printf("원의 둘레 길이: %f\n", (2 * PI * r));
 
     return 0;
