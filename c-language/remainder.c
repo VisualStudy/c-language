34c34fa7b17fdfe169e20cdc649a38da53dbf6e1
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-19 14:28:54 +0000

    remainder

---
+++ c-language/remainder.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int first = 5;
+    int second = 3;
+    int result = 0;
+
+    result = first % second;
+
+    printf("%d\n", result);
+
+    return 0;
+}
