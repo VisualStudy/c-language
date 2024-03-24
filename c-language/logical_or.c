1854a41abc5f62e55484815d0125d51e98ee9e73
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-24 10:51:51 +0000

    logical_or

---
+++ c-language/logical_or.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    printf("%d\n", 1 || 1); // 1
+    printf("%d\n", 1 || 0); // 1
+    printf("%d\n", 0 || 1); // 1
+    printf("%d\n", 0 || 0); // 0
+
+    printf("\n%% %d\n", false || false); // 0
+    
+    return 0;
+}
