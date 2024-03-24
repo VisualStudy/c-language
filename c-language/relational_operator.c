91911c37a7bae3a90d3a25ec8d13c852519f81a1
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-24 10:39:57 +0000

    stdbool header

--- c-language/relational_operator.c
+++ c-language/relational_operator.c
@@ -2,12 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
+#include <stdbool.h>
 
 int main(void)
 {
+    int a = true;
+    int b = false;
+
+    printf("%d, %d\n", a, b);    
+
     printf("%d\n", 3 < 5); // 1(True)
 
     printf("%d\n", 3 > 5); // 0 (False)
 
+    printf("%d\n", 3 <= 5); // 1 (True)
+    
+    printf("%d\n", 3 >= 5); // 0 (False)
+    
+    printf("%d\n", 3 == 5); // 0 (False)
+    
+    printf("%d\n", 3 != 5); // 1 (True)
+    
     return 0;
 }
