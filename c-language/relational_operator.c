6325c6d85dcdb5f2d34666feed0fbd04e3c1ed3a
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-24 10:34:01 +0000

    relationa_operator

---
+++ c-language/relational_operator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+int main(void)
+{
+    printf("%d\n", 3 < 5); // 1(True)
+
+    printf("%d\n", 3 > 5); // 0 (False)
+
+    return 0;
+}
