7d503fff81472c5a2b699563656a38d6472777d7
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-27 14:00:48 +0000

    refactoring

--- c-language/if_not.c
+++ c-language/if_not.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
@@ -9,5 +10,5 @@ int main(void)
         printf("bln: false(0) -> ! -> true(1)\n");
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
