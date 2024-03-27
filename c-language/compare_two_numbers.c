bbbb58f20db6faeb71980c746f5a349ce3a66a6d
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-27 13:53:05 +0000

    compare_two_numbers

---
+++ c-language/compare_two_numbers.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 0;
+    int num2 = 0;
+
+    printf("first num:  "); scanf("%d", &num1);
+    printf("second num:  "); scanf("%d", &num2);
+
+    if (num1 > num2)
+    {
+        printf("%d > %d.\n", num1, num2);
+    }
+    else if (num1 < num2)
+    {
+        printf("%d < %d.\n", num1, num2);
+    }
+    else
+    {
+        printf("%d = %d.\n", num1, num2);
+    }
+    return 0;
+}
