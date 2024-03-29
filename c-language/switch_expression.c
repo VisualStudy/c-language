5202e8a1628eb531844fa94142606c6c4f0e4c10
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-29 13:15:30 +0000

    refactoring

--- c-language/switch_expression.c
+++ c-language/switch_expression.c
@@ -4,18 +4,18 @@ int main(void)
 {
     int x = 3;
 
-	switch (x)
-	{
-		case 1:
-			printf("This is 1.\n");
-			break;
-		case 2:
-			printf("This is 2.\n");
-			break;
-		default:
-			printf("not 1 or 2.\n");
-			break;
-	}
+    switch (x)
+    {
+    case 1:
+        printf("This is 1.\n");
+        break;
+    case 2:
+        printf("This is 2.\n");
+        break;
+    default:
+        printf("not 1 or 2.\n");
+        break;
+    }
 
     return 0;
 }
