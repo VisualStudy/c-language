a9d606afab314e5e59f5841b34bde64d946344ac
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-29 13:22:43 +0000

    switch_statement

---
+++ c-language/switch_statement.c
@@ -0,0 +1,37 @@
+﻿
+#include <stdio.h>
+
+int main(void)
+{
+    int choice = 0;
+    int _ = 0;
+
+    printf("가장 좋아하는 프로그래밍 언어는?\n");
+    printf("1. C\t");
+    printf("2. C++\t");
+    printf("3. C#\t");
+    printf("4. Java\n");
+
+    _ = scanf_s("%d", &choice); _ = getchar();
+
+    switch (choice)
+    {
+        case 1:
+            printf("C language\n");
+            break;
+        case 2:
+            printf("Cpp\n");
+            break;
+        case 3:
+            printf("C#\n");
+            break;
+        case 4:
+            printf("Java\n");
+            break;
+        default:
+            printf("Python\n");
+            break;
+    }
+
+    return 0;
+}
