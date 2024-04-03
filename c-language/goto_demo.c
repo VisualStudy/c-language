11fd29a34cd898ebee049f324b788bb5edc2a4d4
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-04-03 12:01:05 +0000

    goto_demo

---
+++ c-language/goto_demo.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int chapter = 0;
+    int r = 0;
+
+    printf("start\n");
+
+Start:
+    printf("0, 1, 2 :   _\a");
+    r = scanf("%d", &chapter);
+
+    if (chapter == 1)
+    {
+        goto Chapter1;
+    }
+    else if (chapter == 2)
+    {
+        goto Chapter2;
+    }
+    else
+    {
+        goto End;
+    }
+
+Chapter1:
+    printf("1page.\n");
+Chapter2:
+    printf("2page.\n");
+    goto Start;
+
+End:
+    printf("END\n");
+
+    return 0;
+}
