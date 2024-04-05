8e770c1f3228a149723c4e2bd5913ae7b1c276c5
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-04-05 14:51:41 +0000

    currency count

---
+++ c-language/tests/currency_count.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+main() {
+    int m = 4620;
+    int a = m / 1000;
+    int b = m % 1000 / 500;
+    int c = m % 500 / 100;
+    int d = m % 100 / 10;
+    printf("1000Won Count : %d\n", a);
+    printf(" 500Won Count : %d\n", b);
+    printf(" 100Won Count : %d\n", c);
+    printf("  10Won Count : %d\n", d);
+}
