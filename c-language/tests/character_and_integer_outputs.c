353416081c0d94cd1983d6913360af2fe7ee1fee
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-04-08 10:37:09 +0000

    answer

--- c-language/tests/character_and_integer_outputs.c
+++ c-language/tests/character_and_integer_outputs.c
@@ -5,9 +5,9 @@ int main(void)
     // ASCII_Code 'A' = 65, 'a' = 97
     int a = 10;
     char b = 'a';
-    printf("%d\n", a);
-    printf("%d\n", b);
-    printf("%c", b);
+    printf("%d\n", a); //10
+    printf("%d\n", b); //97
+    printf("%c", b); //a
     
     return 0;
 }
