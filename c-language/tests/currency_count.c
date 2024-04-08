7b451934ce2e13fe834c6be19649c2dae45398d1
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-04-08 10:53:03 +0000

    .

--- c-language/tests/currency_count.c
+++ c-language/tests/currency_count.c
@@ -8,6 +8,7 @@ int main(void)
     int b = m % 1000 / 500; // b = 4620 % 1000 / 500 => 1
     int c = m % 500 / 100; // c = 4620 % 500 / 100 => 1
     int d = m % 100 / 10; // d = 4620 % 100 / 10 => 2
+    
     printf("1000Won Count : %d\n", a); // 4
     printf(" 500Won Count : %d\n", b); // 1
     printf(" 100Won Count : %d\n", c); // 1
