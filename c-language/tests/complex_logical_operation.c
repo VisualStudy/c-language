25dabe7b5671171e672dd93db1f2afe45343a40b
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-04-08 11:09:03 +0000

    .

--- c-language/tests/complex_logical_operation.c
+++ c-language/tests/complex_logical_operation.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-main() {
+int main(void) {
     int a = 5, b = 10, c = 15, d = 30, result; 
     result = a * 3 + b > d || c - b / a <= d && 1; 
     // result = (a * 3) + (b > d) || c - (b / a) <= d && 1;
