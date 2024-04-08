d6e353056d9b7c72c624460b62c7107c2475c1e2
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-04-08 11:08:36 +0000

    complex_logica_operation

---
+++ c-language/tests/complex_logical_operation.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main() {
+    int a = 5, b = 10, c = 15, d = 30, result; 
+    result = a * 3 + b > d || c - b / a <= d && 1; 
+    // result = (a * 3) + (b > d) || c - (b / a) <= d && 1;
+    // result = (((a * 3) + b) > d) || (c - (b / a)) <= d && 1;
+    // result = (((15) + 10) > 30) || (13) <= 30 && 1;
+    // result = FALSE || TRUE && 1;
+    // result = TRUE && 1(TRUE); => TRUE = 1
+    printf("%d\n", result); // 1
+}
