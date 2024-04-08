b20575473734f8ddefbc969bacc21d250c05a697
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-04-08 10:52:27 +0000

    currency_count

--- c-language/tests/currency_count.c
+++ c-language/tests/currency_count.c
@@ -4,14 +4,14 @@
 int main(void) 
 {
     int m = 4620;
-    int a = m / 1000;
-    int b = m % 1000 / 500;
-    int c = m % 500 / 100;
-    int d = m % 100 / 10;
-    printf("1000Won Count : %d\n", a);
-    printf(" 500Won Count : %d\n", b);
-    printf(" 100Won Count : %d\n", c);
-    printf("  10Won Count : %d\n", d);
+    int a = m / 1000; // a = 4620 / 1000 => 4
+    int b = m % 1000 / 500; // b = 4620 % 1000 / 500 => 1
+    int c = m % 500 / 100; // c = 4620 % 500 / 100 => 1
+    int d = m % 100 / 10; // d = 4620 % 100 / 10 => 2
+    printf("1000Won Count : %d\n", a); // 4
+    printf(" 500Won Count : %d\n", b); // 1
+    printf(" 100Won Count : %d\n", c); // 1
+    printf("  10Won Count : %d\n", d); // 2
 
     return 0;
 }
