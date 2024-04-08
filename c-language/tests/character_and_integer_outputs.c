22366ae77edf58f074554a822c629dc470e84378
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-04-08 10:35:07 +0000

    refactoring

--- c-language/tests/character_and_integer_outputs.c
+++ c-language/tests/character_and_integer_outputs.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-main() {
+int main(void) 
+{
     // ASCII_Code 'A' = 65, 'a' = 97
     int a = 10;
     char b = 'a';
     printf("%d\n", a);
     printf("%d\n", b);
     printf("%c", b);
+    
+    return 0;
 }
