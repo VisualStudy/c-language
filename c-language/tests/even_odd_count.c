76c9ac5080446e14b4283fdfaa2af10aaaba63f3
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-04-12 07:27:00 +0000

    refactoring

--- c-language/tests/even_odd_count.c
+++ c-language/tests/even_odd_count.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
     int num = 35, evencnt = 0, oddcnt = 0;
-    for (int i = 1; i <= num; i++) 
+    for (int i = 1; i <= num; i++)
     {
         if (i % 2 == 0)
+        {
             evencnt++;
+        }
         else
+        {
             oddcnt++;
+        }
     }
 
     printf("%d %d", evencnt, oddcnt); // 17 18
