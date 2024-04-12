e9b6a6bf7227bae94e334acf70ce69c96361df67
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-04-12 07:25:53 +0000

    .

--- c-language/tests/even_odd_count.c
+++ c-language/tests/even_odd_count.c
@@ -3,7 +3,8 @@
 int main(void) 
 {
     int num = 35, evencnt = 0, oddcnt = 0;
-    for (int i = 1; i <= num; i++) {
+    for (int i = 1; i <= num; i++) 
+    {
         if (i % 2 == 0)
             evencnt++;
         else
