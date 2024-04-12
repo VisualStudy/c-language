93906781296549e6cded92639b71fc14685afb93
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-04-12 07:32:50 +0000

    refactoring

--- c-language/tests/sum_of_multiples_of_seven.c
+++ c-language/tests/sum_of_multiples_of_seven.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
-main() {
+int main(void) 
+{
     int i = 1, n = 0;
-    while (i <= 50) {
-        if (i % 7 == 0)
+    
+    while (i <= 50) 
+    {
+        if (i % 7 == 0) 
+        {
             n += i;
+        }
         i++;
     }
     printf("%d", n);
+
+    return 0;
 }
