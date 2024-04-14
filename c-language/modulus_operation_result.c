443a8b60285e1aa8d3715a7d8d49014ef937a014
VisualStudy <sparksoldier@outlook.com>
2024-04-14 19:00:01 +0900

    refactoring

--- c-language/modulus_operation_result.c
+++ c-language/modulus_operation_result.c
@@ -1,9 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS // scanf 보안 경고 무시
 #include <stdio.h>
 
-main() {
+int main(void) 
+{
     int a, b, r;
     scanf("%d %d", &a, &b);
     r = a % b;
     printf("%d", r);
+
+    return 0;
 }
