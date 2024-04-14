98b5feb9c6469ecf9bf67b6f9439cec0dfd84a60
VisualStudy <sparksoldier@outlook.com>
2024-04-14 19:07:14 +0900

    refactoring

--- c-language/increment_decrement_operation.c
+++ c-language/increment_decrement_operation.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-main() {
+int main(void) 
+{
     int num1 = 16, num2 = 44;
     int a = num1++; // a = 16, num1 = 17
     int b = --num2; // b = 43, num2 = 43 
     printf("%d", a + b); // 59
+
+    return 0;
 }
