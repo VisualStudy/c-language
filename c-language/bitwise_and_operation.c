1698e5327b9152466eaa25a09e74172b6f7e5506
VisualStudy <sparksoldier@outlook.com>
2024-04-15 22:07:04 +0900

    .

--- c-language/bitwise_and_operation.c
+++ c-language/bitwise_and_operation.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) 
 {
@@ -8,5 +9,5 @@ int main(void)
     result = a & b; // 0100 = 4
     printf("%d", result); // 4
 
-    return 0;
+    return EXIT_SUCCESS;
 }
