8e11e770a1cb08643dd1f2a446fa3e8d1324170e
VisualStudy <sparksoldier@outlook.com>
2024-04-16 21:40:26 +0900

    .

--- c-language/bitwise_right_shift_operation.c
+++ c-language/bitwise_right_shift_operation.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-main() {
+int main(void) 
+{
     int num = 1640;
     printf("%d\n", num >> 1); // 1640 / (2^1) = 820
     printf("%d\n", num >> 2); // 1640 * (1 / 2^2) = 410
     printf("%d\n", num >> 3); // 1640 * (1 / 2^3) = 205
+
+    return 0;
 }
