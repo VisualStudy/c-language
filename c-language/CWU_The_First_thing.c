ecfb7503a6ad29ea578c8ec123261996efdecc2d
VisualStudy <sparksoldier@outlook.com>
2024-04-17 21:56:53 +0900

    The_First_thing

---
+++ c-language/CWU_The_First_thing.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // 모니터 화면, 마우스, 키보드 등 기본 입출력 장치 위해
+#include <stdlib.h> // 스탠다드 라이브러리에 담긴 기능을 위하여
+#pragma warning (disable:4996) // 기본적으로 c++를 c처럼 쓰기 위해서
+//하지만 굳이 c++로 c를 돌릴 필요는 없지 않을까?
+
+void main()
+{
+
+}
