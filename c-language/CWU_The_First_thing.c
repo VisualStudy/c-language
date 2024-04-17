7d1ab07f055a9d265c095422d91f58ad0f7bab5e
VisualStudy <sparksoldier@outlook.com>
2024-04-17 22:02:56 +0900

    refactoring

--- c-language/CWU_The_First_thing.c
+++ c-language/CWU_The_First_thing.c
@@ -1,5 +1,8 @@
 #include <stdio.h> // 모니터 화면, 마우스, 키보드 등 기본 입출력 장치 위해
 #include <stdlib.h> // 스탠다드 라이브러리에 담긴 기능을 위하여
+#include <memory.h> // 동적 할당
+#include <time.h> // 시간 다루기
+#include <windows.h> // 윈도우 운영체제 제어(다른 운영체제에선 X)
 #pragma warning (disable:4996) // 기본적으로 c++를 c처럼 쓰기 위해서
 //하지만 굳이 c++로 c를 돌릴 필요는 없지 않을까?
 
---
+++ c-language/CWU_While_Scanf_Case.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h> // 동적 할당
+#pragma warning (disable:4996) // c++은 해당 작업으로 c로 사용 가능
+
+void main()
+{
+    int i, menu;
+    while (1) // 무한루프
+    {
+        printf("\n\n\n 메뉴 1) 2) 3) 4) 5) 6) 7)종료: ");
+        scanf()
+    }
+}
