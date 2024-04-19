3e752f2b0329198e47dbb985c1541c52f4890ed3
VisualStudy <sparksoldier@outlook.com>
2024-04-19 21:32:30 +0900

    sizeof_operator

--- c-language/signed_integer.c
+++ c-language/signed_integer.c
@@ -12,5 +12,14 @@ int main(void)
     printf("long: %ld\n", l);
     printf("long long: %lld\n", ll);
 
+    printf("%llu %llu %llu %llu %llu %llu\n",
+        sizeof(short), 
+        sizeof(int), 
+        sizeof(long), 
+        sizeof(long long),
+        sizeof(float),
+        sizeof(double)
+        );
+
     return 0;
 }
