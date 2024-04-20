84898907999fa91605687ab48b30badd2882894a
VisualStudy <sparksoldier@outlook.com>
2024-04-20 21:56:23 +0900

    .

--- c-language/integer_size.c
+++ c-language/integer_size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
@@ -6,5 +7,6 @@ int main(void)
     printf("int: %lld\n", sizeof(int));
     printf("long: %lld\n", sizeof(long));
     printf("unsigned: %lld\n", sizeof(unsigned int));
-    return 0;
+
+    return EXIT_SUCCESS;
 }
