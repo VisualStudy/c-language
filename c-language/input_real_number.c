1163c45b7d1b40063e0b6f93e838fe2ffc15b4f7
VisualStudy <sparksoldier@outlook.com>
2024-04-20 21:50:10 +0900

    discard variable

--- c-language/input_real_number.c
+++ c-language/input_real_number.c
@@ -6,8 +6,9 @@ int main(void)
     float num1;
     double num2;
     long double num3;
+    int _; // discard
 
-    scanf("%f %lf %Lf", &num1, &num2, &num3);
+    _ = scanf("%f %lf %Lf", &num1, &num2, &num3);
 
     printf("%.15f\n%.15lf\n%.15Lf\n", num1, num2, num3);
 
