a1023d69967e370b299b2f6f18c5f31d05793328
VisualStudy <sparksoldier@outlook.com>
2024-04-22 22:53:31 +0900

    .

--- c-language/binary_to_decimal.c
+++ c-language/binary_to_decimal.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
     int input = 101110; // 2 + 4 + 8 + 32 = 46
     int di = 1;
     int sum = 0;
-    while (1) 
+    while (1)
     {
-        if (input == 0) break;
+        if (input == 0)
+        {
+            break;
+        }
         sum = sum + (input % 10) * di;
         di = di * 2;
         input = input / 10;
