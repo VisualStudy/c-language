697814f9d276a59709a18ee833ba8654111fa6bc
VisualStudy <sparksoldier@outlook.com>
2024-04-22 22:49:59 +0900

    binary_to_decimal

---
+++ c-language/binary_to_decimal.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main() {
+    int input = 101110; // 2 + 4 + 8 + 32 = 46
+    int di = 1;
+    int sum = 0;
+    while (1) {
+        if (input == 0) break;
+        sum = sum + (input % 10) * di;
+        di = di * 2;
+        input = input / 10;
+    }
+    printf("%d", sum);
+}
