71c755e9bf4dfa1476f30e70ee3132b48a2b0903
VisualStudy <sparksoldier@outlook.com>
2024-04-22 22:52:47 +0900

    .

--- c-language/binary_to_decimal.c
+++ c-language/binary_to_decimal.c
@@ -5,7 +5,8 @@ int main(void)
     int input = 101110; // 2 + 4 + 8 + 32 = 46
     int di = 1;
     int sum = 0;
-    while (1) {
+    while (1) 
+    {
         if (input == 0) break;
         sum = sum + (input % 10) * di;
         di = di * 2;
