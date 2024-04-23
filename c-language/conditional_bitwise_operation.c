f6135e5ce8b5e79edd18cc07d343e868f65adbf8
VisualStudy <sparksoldier@outlook.com>
2024-04-23 20:22:47 +0900

    .

--- c-language/conditional_bitwise_operation.c
+++ c-language/conditional_bitwise_operation.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-main() 
+int main(void) 
 {
     int num1 = 16, num2 = 80; 
     // num1 = 00010000, 
     // num2 = 01010000
     //        01000000 = 64
     printf("%d", num1 > num2 ? num1 & num2 : num1 ^ num2);
+
+    return 0;
 }
