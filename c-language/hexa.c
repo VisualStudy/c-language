2c1c23971a69fb5022c031c9450401f3ea0d963e
VisualStudy <sparksoldier@outlook.com>
2024-04-24 22:51:16 +0900

    hexa

---
+++ c-language/hexa.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 010; // 8
+    int y = 0x10; // 16
+
+    printf("%d\n", x);
+    printf("%d\n", y);
+
+    return 0;
+}
---
+++ c-language/unsigned_int.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned int sales = 3000000000;
+
+    printf("%u\n", sales);
+
+    return 0;
+}
