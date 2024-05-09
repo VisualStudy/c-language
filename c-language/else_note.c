8a8bb9b6e2f8c1bd5d3173589a8dab57da4ee01d
VisualStudy <sparksoldier@outlook.com>
2024-05-09 14:47:36 +0900

    else_note

---
+++ c-language/else_note.c
@@ -0,0 +1,16 @@
+// else 문의 기본 사용 예시
+#include <stdio.h>
+
+int main(void)
+{
+    if (1 != 1) // 거짓 조건
+    {
+        // 조건이 참일 경우 실행될 코드
+    }
+    else
+    {
+        printf("조건식이 거짓이기에 현재 문장이 실행됩니다.\n");
+    }
+
+    return 0;
+}
