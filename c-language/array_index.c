5dbfaf2f656ecd44ce8cd8a1d3a77ca9f7ee30ba
VisualStudy <sparksoldier@outlook.com>
2024-05-16 21:40:52 +0900

    .

--- c-language/array_index.c
+++ c-language/array_index.c
@@ -7,6 +7,7 @@ int main(void)
     int array[] = { 1, 2, 3 };
     int index = 0; // 배열의 인덱스는 0부터 시작하기에 0으로 index 변수 초기화 
     int temp = 0;
+    int _ = 0;
 
     printf("%d\t", array[index++]); // array[0] 출력 후, index == 1로 증가
     printf("%d\t", array[index++]); // array[1] 출력 후, index == 2로 증가
@@ -15,7 +16,8 @@ int main(void)
     printf("%d\t", array[--index]); // index == 2로 감소 후, array[2] 출력
     temp = --index;
     printf("%d\t", array[temp]); // index == 1로 감소 후, array[1] 출력
-    printf("%d\n", array[--index]); // index == 0로 감소 후, array[0] 출력
+    _ = --index;
+    printf("%d\n", array[_]); // index == 0로 감소 후, array[0] 출력
 
     return 0;
 }
