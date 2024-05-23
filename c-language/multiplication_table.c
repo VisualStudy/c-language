991d2f60d25b3c913154fde68ac12c7dd6a4d715
VisualStudy <sparksoldier@outlook.com>
2024-05-23 23:04:05 +0900

    multiplication_table

---
+++ c-language/multiplication_table.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0; // 단 입력 받을 값 초기화
+    int _ = 0; // scanf 경고 무시
+    printf("원하는 단 입력 :  ");
+    _ = scanf("%d", &i);
+
+    for (int j = 1; j <= 9; j++) // 입력 받은 단에 1 ~ 9까지 곱하기
+    {
+        printf("\n %d * %d = %d", i, j, i * j);
+    }
+
+    return 0;
+}
