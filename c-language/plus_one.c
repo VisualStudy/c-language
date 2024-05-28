86c553c7839b918b6a08afdfd08d0dc66ebf4e24
VisualStudy <sparksoldier@outlook.com>
2024-05-28 15:27:37 +0000

    plus_one

---
+++ c-language/plus_one.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#pragma warning (disable:4996)
+
+int main(void)
+{
+    int _ = 0;
+    int i = 0;
+    int sum = 0;
+    printf("<1을 더해드립니다!>\n\n");
+    printf("임의의 숫자를 입력 :  ");
+    _ = scanf("%d", &i);
+
+    sum = i + 1;
+    printf("%d", sum);
+
+    return 0;
+}
