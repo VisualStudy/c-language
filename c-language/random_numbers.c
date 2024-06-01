f062ff72eba5d6a867abc4f43721233081dbc97c
VisualStudy <sparksoldier@outlook.com>
2024-06-01 22:45:50 +0900

    .

--- c-language/random_numbers.c
+++ c-language/random_numbers.c
@@ -8,6 +8,7 @@ int main(void) // 1~100 사이의 난수를 가정하고 이분검색 방식으로 찾아가기
 {
     srand((unsigned)time(NULL)); //현재 시간 기준 시드값 설정하기
     
+    int _ = 0;
     int answer = 0;
 
     int cnt = 0;
@@ -17,7 +18,7 @@ int main(void) // 1~100 사이의 난수를 가정하고 이분검색 방식으로 찾아가기
     do
     {
         printf("\n 추측값 입력 :  ");
-        scanf("%d", &guess);
+        _ = scanf("%d", &guess);
         if (answer < guess)
         {
         printf("\n 입력값이 더 높습니다.");
