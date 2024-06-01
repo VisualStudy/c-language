85f1c46a4079254e8fd481010562a60f30088c70
VisualStudy <sparksoldier@outlook.com>
2024-06-01 22:45:09 +0900

    random_numbers

---
+++ c-language/random_numbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#pragma warning (disable:4996)
+
+int main(void) // 1~100 사이의 난수를 가정하고 이분검색 방식으로 찾아가기
+{
+    srand((unsigned)time(NULL)); //현재 시간 기준 시드값 설정하기
+    
+    int cnt = 0;
+    int guess = 0;
+    int answer = (rand() % 100) + 1;
+
+    do
+    {
+        printf("\n 추측값 입력 :  ");
+        scanf("%d", &guess);
+        if (answer < guess)
+        {
+        printf("\n 입력값이 더 높습니다.");
+        }
+
+        if (answer > guess)
+        {
+            printf("\n 입력값이 더 낮습니다.");
+        }
+        cnt++;
+    }
+    
+    while (answer != guess);
+    printf("\n 정답! <추측횟수> = %d", cnt);
+
+    return 0;   
+}
