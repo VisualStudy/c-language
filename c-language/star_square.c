075f6001b9b99e2236fcb8f33de3d0d83969b9ab
VisualStudy <sparksoldier@outlook.com>
2024-06-02 22:58:35 +0900

    .

--- c-language/star_square.c
+++ c-language/star_square.c
@@ -4,38 +4,24 @@
 
 int main(void) // 별표 사각형 ( 10 X 50 ) 작성
 {
+	int i = 0;
 	int count = 0;
 	int prime[N];
 
-	for (int i = 0; i < N; i++) // 배열요소에 1 입력 (소수로 가정)
-	{
+	for (i = 0; i < N; i++) // 배열요소에 1 입력(소수로 가정)
 		prime[i] = 1;
-	}
-
-	prime[0] = prime[1] = 0; // 0~1은 소수에서 제외
-	for (int i = 2; i < N; i++) // 2부터 시작
-	{
+	prime[0] = prime[1] = 0; //0~1은 소수에서 제외
+	for (i = 2; i < N; i++) // 2부터 시작한다.
 		if (prime[i] == 1) // 소수인 경우에만 작동
-		{
 			for (int j = 2; j < N; j++)
-			{
-				if (i * j < N)
-				{
-					prime[i * j] = 0;
-				}
-			}
-		}
-	}
-	for (int i = 2; i < N; i++)
+				if (i * j < N) prime[i * j] = 0;
+	for (i = 2; i < N; i++)
 	{
-		if (prime[i] == 1) // i가 소수라면?
+		if (prime[i] == 1)  // i가 소수이면?
 		{
-			printf("%3d", i);
+			printf("%3d ", i);
 			count++;
-			if (count % 10 == 0)
-			{
-				printf("\n");
-			}
+			if (count % 10 == 0) printf("\n");
 		}
 	}
 
