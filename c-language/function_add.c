3dd0c220f0a4c09ae7bb057120de93724bf82aac
VisualStudy <sparksoldier@outlook.com>
2024-06-19 15:15:52 +0900

    function_add

---
+++ c-language/function_add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//함수 선언
+int add(int a, int b);
+
+//함수 정의
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main(void)
+{
+    int result = add(3, 4); // 함수 호출
+    printf("결과 : %d\n", result); // 결과: 7
+    return 0;
+}
