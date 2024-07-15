83bffbb612dbefcf4ddb70ce824f4580a359313a
VisualStudy <sparksoldier@outlook.com>
2024-07-15 19:05:48 +0900

    .

---
+++ c-language/quiz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    while (1)
+    {
+        printf("다음 중 생물학적 과일이 아닌 것은? 1) 수박 2) 토마토 3) 오이 4) 루바브");
+        scanf("%d", &a);
+        switch (a)
+        {
+            case(1):
+            {
+                printf("오답");
+                break;
+            }
+            case(2):
+            {
+                printf("오답");
+                break;
+            }
+        }
+    }
+
+
+    return 0;
+}
