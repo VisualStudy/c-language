5d3ed7fd4864f824cc9c691832cdda39d6150654
VisualStudy <sparksoldier@outlook.com>
2024-08-06 19:42:09 +0900

    .

--- c-language/gambler.c
+++ c-language/gambler.c
@@ -4,29 +4,29 @@
 #include <time.h>
 #include <string.h>
 
-void 던지기();
-void 결과확인(int 동전상태);
+void toss();
+void checkResult(int coinState);
 
-int main() 
+int main()
 {
-    char 명령어[20];
+    int command;
     srand(time(0)); // 랜덤 시드 설정
 
-    while (1) 
+    while (1)
     {
-        printf("명령어를 입력하세요 ('던져! 던져!' 또는 '종료' 입력 가능): ");
-        scanf("%s", 명령어);
+        printf("명령어를 입력하세요 (1: '던져! 던져!', 2: '종료'): ");
+        scanf("%d", &command);
 
-        if (strcmp(명령어, "던져! 던져!") == 0) 
+        if (command == 1)
         {
-            던지기();
+            toss();
         }
-        else if (strcmp(명령어, "종료") == 0) 
+        else if (command == 2)
         {
             printf("게임을 종료합니다.\n");
             break;
         }
-        else 
+        else
         {
             printf("알 수 없는 명령어입니다. 다시 입력하세요.\n");
         }
@@ -35,25 +35,25 @@ int main()
     return 0;
 }
 
-void 던지기() 
+void toss()
 {
-    int 동전상태 = rand() % 2; // 0 또는 1 무작위 생성
-    결과확인(동전상태);
+    int coinState = rand() % 2; // 0 또는 1 무작위 생성
+    checkResult(coinState);
 }
 
-void 결과확인(int 동전상태) 
+void checkResult(int coinState)
 {
-    char 추측[10];
+    char guess[10];
 
-    printf("동전의 앞뒷면을 맞춰보세요 ('앞면' 또는 '뒷면'): ");
-    scanf("%s", 추측);
+    printf("동전의 앞뒷면을 맞춰보세요 ('앞면' 또는 '뒷면'을 직접 입력하시오. 단, '을 제외하고 입력할 것.): ");
+    scanf("%s", guess);
 
-    if ((동전상태 == 1 && strcmp(추측, "앞면") == 0) ||
-        (동전상태 == 0 && strcmp(추측, "뒷면") == 0)) 
-        {
+    if ((coinState == 1 && strcmp(guess, "앞면") == 0) ||
+        (coinState == 0 && strcmp(guess, "뒷면") == 0))
+    {
         printf("정답!\n");
     }
-    else 
+    else
     {
         printf("오답!\n");
     }
