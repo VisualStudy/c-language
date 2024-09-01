ac6469d0b84e52505975d2a2c7147e460e10051c
VisualStudy <sparksoldier@outlook.com>
2024-09-01 18:35:57 +0900

    main

--- c-language/simple_c_calculator.c
+++ c-language/simple_c_calculator.c
@@ -6,3 +6,9 @@ float add(float a, float b);
 float subtract(float a, float b);
 float multiply(float a, float b);
 float divide(float a, float b);
+
+int main(voide)
+{
+    float num1, num2, result;
+    int choice;
+}
