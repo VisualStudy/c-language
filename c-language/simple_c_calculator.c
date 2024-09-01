8987c21d93c3b5baf5f67caa27bfe1cae9848ab5
VisualStudy <sparksoldier@outlook.com>
2024-09-01 18:39:01 +0900

    while basic

--- c-language/simple_c_calculator.c
+++ c-language/simple_c_calculator.c
@@ -11,4 +11,25 @@ int main(voide)
 {
     float num1, num2, result;
     int choice;
+
+    while (1)
+    {
+        printMenu(); // 메뉴 출력
+
+        printf("원하는 연산을 선택하시오 (1-4) 또는 0을 입력하여 종료:  ");
+        scanf("%d", &choice);
+
+        if (choice == 0)
+        {
+            printf("프로그램을 종료합니다.\n");
+            break;
+        }
+
+        printf("첫 번째 숫자를 입력하세요: ");
+        scanf("%f", &num1);
+        printf("두 번째 숫자를 입력하세요: ");
+        scanf("%f", &num2);
+
+        
+    }
 }
