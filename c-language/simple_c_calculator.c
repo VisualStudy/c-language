1efca663432c303f0aed18455f3d9717b88d015a
VisualStudy <sparksoldier@outlook.com>
2024-09-01 18:35:16 +0900

    function

--- c-language/simple_c_calculator.c
+++ c-language/simple_c_calculator.c
@@ -1,1 +1,8 @@
 #include <stdio.h>
+
+// 함수 선언
+void printMenu();
+float add(float a, float b);
+float subtract(float a, float b);
+float multiply(float a, float b);
+float divide(float a, float b);
