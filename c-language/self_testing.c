fd0d66b45489008a29f0fc89d595aa29c1995f13
VisualStudy <sparksoldier@outlook.com>
2024-10-29 23:15:57 +0800

    clear

--- c-language/self_testing.c
+++ c-language/self_testing.c
@@ -4,15 +4,6 @@ int main(void)
 {
     printf("셀프 테스팅에 오신 것을 환영합니다!");
     printf("----------------------------------");
-    printf("Let's go~");
-    printf("dori");
-    printf("dodori");
-    printf("godori");
-    printf("apt");
-    printf("hello, world!");
-    printf("bba ba bam");
-    printf("lala");
-    printf("hello~ how are you~ how are you today!");
 
     return 0;
 }
