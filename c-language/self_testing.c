eb33c2afb102a85e67f4f6d2b65c3e13fb95a957
VisualStudy <sparksoldier@outlook.com>
2024-11-23 23:57:58 +0900

    .

--- c-language/self_testing.c
+++ c-language/self_testing.c
@@ -2,12 +2,14 @@
 
 int main(void)
 {
+    char a = a;
+    
     printf("셀프 테스팅에 오신 것을 환영합니다!");
     printf("----------------------------------");
     printf("a");
     printf("b");
     printf("c");
-
+    printf("%c", a);
 
 
     return 0;
