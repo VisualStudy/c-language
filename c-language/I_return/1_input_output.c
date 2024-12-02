e81b6f5354ad4aea0720e0a07fa6e89d48586cd4
VisualStudy <sparksoldier@outlook.com>
2024-12-02 13:13:07 +0900

    return project_1_input_output

---
+++ c-language/I_return/1_input_output.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // 기본
+// c에서 코드 대부분은 ;(세미콜론: semicolon)을 마지막에 적어줘야 작동
+int main(void) // 표준 규격(모든 정보 표기로 가독성 up)
+{
+    printf("Hello,, World!\n"); // printf로 콘솔창에 쌍따옴표 안 내용을 출력
+                                // \n으로 한 줄 띄기
+    return 0;
+}
