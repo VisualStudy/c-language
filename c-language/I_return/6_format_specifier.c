98f3e20470dad51ee12506efe7a4826524cc803a
VisualStudy <sparksoldier@outlook.com>
2024-12-03 21:03:38 +0900

    .

--- c-language/I_return/6_format_specifier.c
+++ c-language/I_return/6_format_specifier.c
@@ -7,16 +7,16 @@ int main(void)
     char letter = 'A';
     char str[] = "Hello, C programming!";
 
-    printf("Integer: %d\n", num);
-    printf("Unsigned Integer: %u\n", num);
+    printf("Integer: %d\n", num); // %d 정수
+    printf("Unsigned Integer: %u\n", num); // %u 부호 없는 정수
 
-    printf("Hexadecimal (lowercase): %x\n", num);
-    printf("Hexadceimal (uppercase): %X\n", num);
+    printf("Hexadecimal (lowercase): %x\n", num); // %x 16진수 소문자
+    printf("Hexadceimal (uppercase): %X\n", num); // %X 16진수 대문자
 
-    printf("Floating point: %f\n", pi);
+    printf("Floating point: %f\n", pi); // %f: 부동소수점(float, double)
 
-    printf("character: %c\n", letter);
-    printf("String: %s\n", str);
+    printf("character: %c\n", letter); // %c 문자
+    printf("String: %s\n", str);      //%s 문자열
 
     return 0;
 }
