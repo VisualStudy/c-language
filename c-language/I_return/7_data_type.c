f9ef0d07195abbaf99e468463bdb64ce60e1ba96
VisualStudy <sparksoldier@outlook.com>
2024-12-06 17:27:48 +0900

    .

--- c-language/I_return/7_data_type.c
+++ c-language/I_return/7_data_type.c
@@ -8,13 +8,13 @@ int main(void)
     double d = 2.718281828;
     char e = 'Z';
 
-    printf("Signed Integer: %d\n", a);
-    printf("Unsigned Integer: %u\n", b);
+    printf("Signed Integer: %d\n", a); // 부호가 있는 정수형
+    printf("Unsigned Integer: %u\n", b); // 부호가 없는 정수형
 
-    printf("Float: %.2f\n", c);
+    printf("Float: %.2f\n", c); // 소수 2 번째 자리까지 
 
-    printf("Double: %.9f\n", d);
-    printf("Character: %c\n", e);
+    printf("Double: %.9f\n", d); // 소수 9 번째 자리까지
+    printf("Character: %c\n", e); // 문자
     
     return 0;
 }
