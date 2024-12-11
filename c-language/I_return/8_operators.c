41bb73f67cf92a19c8c2cbcd6b77cd4787d3d766
VisualStudy <sparksoldier@outlook.com>
2024-12-11 19:49:46 +0900

    .

--- c-language/I_return/8_operators.c
+++ c-language/I_return/8_operators.c
@@ -19,11 +19,11 @@ int main(void)
     printf("a > 0 && b > 0: %d\n", (a > 0 && b > 0)); // a > 0 && b > 0: a는 0보다 크다 그리고 b도 0보다 크다
     printf("a > 0 || b < 0: %d\n\n", (a > 0 || b < 0)); // a > 0 || b < 0: a는 0보다 크다 또는 b는 0보다 작다
 
-    printf("Bitwise Operators(비트 연산자):\n"); // 비트 연산자 설명 추가 예정
-    printf("Bitwise AND: %d\n", a & b); // 비트 AND
-    printf("Bitwise OR: %d\n", a | b); // 비트 OR
-    printf("Bitwise XOR: %d\n", a ^ b); // 비트 XOR
-    printf("Bitwise NOT (a): %d\n", ~a); // 비트 NOT
+    printf("Bitwise Operators(비트 연산자):\n"); // 비트 단위로 연산 수행: 2진수 변환 후 연산
+    printf("Bitwise AND: %d\n", a & b); // 비트 AND: 2진수 변환 후 두 비트 모두 1일 때만 1로 취급하여 비트값 계산
+    printf("Bitwise OR: %d\n", a | b); // 비트 OR: 2진수 변환 후 두 비트 중 하나라도 1이면 1 -> 비트값 계산
+    printf("Bitwise XOR: %d\n", a ^ b); // 비트 XOR: 2진수 변환 후 두 수의 비트가 다를 때만 1 -> 비트값 계산
+    printf("Bitwise NOT (a): %d\n", ~a); // 비트 NOT: 2진수 변환 후 1을 0으로, 0을 1로 -> 비트값 계산
 
     return 0;
 }
