0f05301da04797610f168f90f9da3a76645693dc
지호 박 <sparksoldier@outlook.com>
2025-03-20 04:32:04 +0200

    praxis

---
+++ c-language/praxis.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 2550;
+    int y = 19248;
+    int sum;
+    
+    sum = x + y;
+    printf("x가 2550이고 y가 19248일 때, 둘의 합은 %d입니다.", sum);
+
+    return 0;
+}
