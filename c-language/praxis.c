fa609aaf195643e404b45eb0b2ea5d142064d08d
지호 박 <sparksoldier@outlook.com>
2025-03-21 22:11:23 +0900

    10d

--- c-language/praxis.c
+++ c-language/praxis.c
@@ -8,6 +8,10 @@ int main(void)
     
     sum = x + y;
     printf("x가 2550이고 y가 19248일 때, 둘의 합은 %d입니다.", sum);
+    
+    int summer;
+    summer = sum + sum;
+    printf("썸머 썸머 데이: %10d", summer);
 
     return 0;
 }
