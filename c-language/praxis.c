f04d490255a38b550c74f709f2ff569b5e41e7a9
지호 박 <sparksoldier@outlook.com>
2025-03-24 19:27:03 +0900

    square

--- c-language/praxis.c
+++ c-language/praxis.c
@@ -7,11 +7,17 @@ int main(void)
     int sum;
     
     sum = x + y;
-    printf("x가 2550이고 y가 19248일 때, 둘의 합은 %d입니다.", sum);
+    printf("x가 2550이고 y가 19248일 때, 둘의 합은 %d입니다.\n", sum);
     
     int summer;
     summer = sum + sum;
-    printf("썸머 썸머 데이: %10d", summer);;
+    printf("썸머 썸머 데이: %10d\n", summer);;
+
+    printf("* * * * *");
+    printf("* * * * *");
+    printf("* * * * *");
+    printf("* * * * *");
+    printf("* * * * *");
 
     return 0;
 }
