3b5bdb9bc2fa503c4c2524deecf2e279669789c1
지호 박 <sparksoldier@outlook.com>
2025-04-01 23:34:06 +0900

    original

---
+++ c-language/sumtime.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, i, sum = 0;
+
+    printf("정수 입력: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+
+    printf("1부터 %d까지의 합은 %d입니다.\n", n, sum);
+
+    return 0;
+}
