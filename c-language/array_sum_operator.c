fe5769772c1a3bcc1df020392946a07d791a49a6
지호 박 <sparksoldier@outlook.com>
2025-04-28 21:27:25 +0900

    using operator

---
+++ c-language/array_sum_operator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int sum_array[5] = { 10, 20, 30, 40, 50 };
+
+    int sum = 0;
+
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        sum += sum_array[i];
+    }
+    printf("오와!! 최종 합계는 %d이다~~", sum);
+
+    return 0;
+}
