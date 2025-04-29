3c8c72021d19dc7e79df7077fd8ddcf9727a28e1
지호 박 <sparksoldier@outlook.com>
2025-04-29 23:47:38 +0900

    local_variable_with_function_i++

--- c-language/local_variable_with_function.c
+++ c-language/local_variable_with_function.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int function_local(int i)
-{
+{   
     printf("코레가 지역변수 데스: %d\n", i);
+    i++;
 }
 
 int main(void)
@@ -12,6 +13,7 @@ int main(void)
     printf("디스 이스 메인즈 아이: %d\n", i);
     printf("벗...\n");
     function_local(10);
+    printf("마사카.. i값을 ++ 하였는데 늘어났을까?: %d\n", i);
 
     return 0;
 }
