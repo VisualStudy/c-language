82a603b27164705f61cce5b305b383bed4b445da
VisualStudy <sparksoldier@outlook.com>
2025-05-05 03:08:45 +0900

    refactoring

--- c-language/array_pointer_example.c
+++ c-language/array_pointer_example.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-	int i = 0;
-    int arr[5] = {10, 20, 30, 40 , 50};
+    int i = 0;
+    int arr[5] = { 10, 20, 30, 40, 50 };
 
-	int (*p)[5] = &arr; //&arr[0]; &arr[2];
+    int* p = &arr; //&arr[0]; &arr[2];
 
-	//printf("%d %d %d %d %d\n", *p, *(p + 1), *(p + 2), *(p + 3), *(p + 4));
+    printf("%d %d %d %d %d\n", *p, *(p + 1), *(p + 2), *(p + 3), *(p + 4));
 
-	//for (i = 0; i < 5; i++)
-	//{
-	//	printf("이것은 배열: %d\n", *(p + i));
-	//}
+    for (i = 0; i < 5; i++)
+    {
+        printf("이것은 배열: %d\n", *(p + i));
+    }
 
-	//for (i = 0; i < 5; i++)
-	//{
-	//	printf("이것은 배열: %d\n", arr[i]);
-	//}
+    for (i = 0; i < 5; i++)
+    {
+        printf("이것은 배열: %d\n", arr[i]);
+    }
 
-	return 0;
+    return 0;
 }
