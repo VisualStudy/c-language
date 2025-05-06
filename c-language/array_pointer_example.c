c142eb0f3c4f58cae2d7a3f8958421e375ba7f63
지호 박 <sparksoldier@outlook.com>
2025-05-06 23:39:22 +0900

    refactoring

--- c-language/array_pointer_example.c
+++ c-language/array_pointer_example.c
@@ -5,7 +5,7 @@ int main(void)
     int i = 0;
     int arr[5] = { 10, 20, 30, 40, 50 };
 
-    int* p = &arr; //&arr[0]; &arr[2];
+    int* p = arr; //&arr[0]; &arr[2];
 
     printf("%d %d %d %d %d\n", *p, *(p + 1), *(p + 2), *(p + 3), *(p + 4));
 
