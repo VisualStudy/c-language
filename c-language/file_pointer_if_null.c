b68870e1cbb62d675ad926f3d7d734754ea08702
VisualStudy <sparksoldier@outlook.com>
2025-05-16 15:53:29 +0900

    file pointer if null

---
+++ c-language/file_pointer_if_null.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    FILE *f;
+    f = fopen("file_pointer_test.txt", "w");
+    if(f == NULL)
+    {
+        printf("쥐엔장~ 파일이 안 열려~~");
+        return 1;
+    }
+
+    fprintf(f, "파일에 적.는.다!");
+    fclose(f);
+
+    return 0;
+}
