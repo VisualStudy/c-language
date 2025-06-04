474c077d56e5b5526e587a49b7a0dee780e30dff
지호 박 <sparksoldier@outlook.com>
2025-06-04 22:48:29 +0900

    bit_right

---
+++ c-language/bit_right.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int num = 1640;
+    printf("%d\n", num >> 1); // 1640 / (2^1) = 820
+    printf("%d\n", num >> 2); // 1640 * (1 / 2^2) = 410
+    printf("%d\n", num >> 3); // 1640 * (1 / 2^3) = 205
+
+    return EXIT_SUCCESS; // return 0;과 동일 의미
+}
