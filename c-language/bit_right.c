bc5e2e3437ecb0cdd50d6e54a92a41d880d95790
지호 박 <sparksoldier@outlook.com>
2025-06-10 22:56:08 +0600

    .

--- c-language/bit_right.c
+++ c-language/bit_right.c
@@ -8,5 +8,5 @@ int main(void)
     printf("%d\n", num >> 2); // 1640 * (1 / 2^2) = 410
     printf("%d\n", num >> 3); // 1640 * (1 / 2^3) = 205
 
-    return EXIT_SUCCESS; // return 0;과 동일 의미
+    return EXIT_SUCCESS; // return 0;과 동일 의미;
 };
