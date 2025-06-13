f35011b855fe3412ddf66a2a43bbb8c59dc39a60
지호 박 <sparksoldier@outlook.com>
2025-06-13 19:44:43 +0900

    .

--- c-language/global_local_variable.c
+++ c-language/global_local_variable.c
@@ -6,16 +6,16 @@ void TestScope()
 {
     int Local = 50; // TestScope 함수 내 지역 변수
     printf("TestScope 함수 내:\n");
-    printf("전역 변수 Global = %d (주소: %p)\n", Global);
-    printf("지역 변수 Local = %d (주소: %p)\n", Local);
+    printf("전역 변수 Global = %d \n", Global);
+    printf("지역 변수 Local = %d \n", Local);
 }
 
 int main(void)
 {
     int Local = 10; //메인 함수의 지역 변수
     printf("메인 함수 내: \n");
-    printf("전역 변수 Global = %d (주소: %p)\n", Global);
-    printf("지역 변수 Local = %d (주소: %p)\n", Local);
+    printf("전역 변수 Global = %d \n", Global);
+    printf("지역 변수 Local = %d \n", Local);
 
     TestScope();
 
