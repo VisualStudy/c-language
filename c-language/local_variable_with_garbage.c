d609ab9b753bf3b6f700626425061ff5608caa4f
지호 박 <sparksoldier@outlook.com>
2025-06-13 18:03:00 +0900

    .

--- c-language/local_variable_with_garbage.c
+++ c-language/local_variable_with_garbage.c
@@ -10,6 +10,6 @@ int main(void)
 
 void ShowGarbage()
 {
-    int x; // 초기화하지 않은 showGarbage 함수 안 지역 변수
+    int x; // 초기화하지 않은 ShowGarbage 함수 안 지역 변수
     printf("초기화되지 않은 지역 변수 x의 값: %d\n", x);
 }
