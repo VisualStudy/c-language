e902effbd70f4e35053915e7dc69043edf6c36d5
지호 박 <sparksoldier@outlook.com>
2025-09-01 12:58:18 +0900

    .

--- c-language/print_parkour.c
+++ c-language/print_parkour.c
@@ -3,6 +3,9 @@
 int main(void)
 {
     printf("Welcome my Parkour Club!");
+    printf("\n-----------------------------------\n");
+    printf("Please answer this question\n");
+    printf("First, You enjoy Physical Activity?");
 
     return 0;
 }
