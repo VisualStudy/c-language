f217ce61f065bfdc9b047896cd2e73dd31b5c21f
지호 박 <sparksoldier@outlook.com>
2025-09-02 23:03:49 +0900

    ASCII

---
+++ c-language/ASCII.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char x;
+    x = 'a';
+    
+    printf("%d\n", x);
+    printf("%c\n", x);
+    
+    printf("-----------\n");
+
+    int y;
+    y = 97;
+
+    printf("%d\n", y);
+    printf("%c\n", y);
+
+    printf("-----------------\n");
+
+    char z;
+    z = 'a';
+    z = z + 1;
+
+    printf("%d\n", z);
+    printf("%c\n", z);
+
+    printf("------\n");
+
+    z = z - 32;
+
+    printf("%d\n", z);
+    printf("%c\n", z);
+
+    printf("----------\n");
+
+    z = z - 31;
+
+    printf("%d\n", z);
+    printf("%c\n", z);
+
+    return 0;
+}
