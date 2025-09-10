de0cb8163b99a55184b72521c239f72becdbde39
지호 박 <sparksoldier@outlook.com>
2025-09-11 00:00:26 +0900

    ascii_a

--- c-language/ascii_a.c
+++ c-language/ascii_a.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+
+int main(void)
+{
+    int x;
+    x = 97;
+    
+    char y;
+    y = 'a';
+
+    printf("97을 문자로: %c\n", x);
+    printf("문자 a를 숫자로: %d\n", y);
+
+    return 0;
+}
