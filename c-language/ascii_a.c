7c8d96c96dccf9cde3540b95c4628a9b90c5cc87
지호 박 <sparksoldier@outlook.com>
2025-09-13 23:41:35 +0900

    .

--- c-language/ascii_a.c
+++ c-language/ascii_a.c
@@ -10,6 +10,8 @@ int main(void)
 
     char str[4] = {"a, b, c, \0"}; // 문자열은 char 배열로
     char str[3] = {"abcdef"};      // 오버플로
+    char str[4] = {"abc\0"};
+    char str[4] = {"abc"};
 
     printf("97을 문자로: %c\n", x);
     printf("문자 a를 숫자로: %d\n", y);
