bd41c929dbee883caa38868189fc29d268602f7c
지호 박 <sparksoldier@outlook.com>
2025-09-16 21:24:47 +0900

    .

--- c-language/ascii_a.c
+++ c-language/ascii_a.c
@@ -14,6 +14,7 @@ int main(void)
     char str[4] = {"abc"};
     char str[7] = {"abcd e"};
     char str[100] = {"여유롭게 100 정도"};
+    char str[] = {"라바라타 이스터페리움"}; // 파싱 과정에게 맡기기
 
     printf("97을 문자로: %c\n", x);
     printf("문자 a를 숫자로: %d\n", y);
