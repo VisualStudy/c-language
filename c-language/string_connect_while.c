ea81f8607692f3c0bbdaadf321a9929ff2cf223d
지호 박 <sparksoldier@outlook.com>
2025-09-22 02:15:15 +0900

    null

--- c-language/string_connect_while.c
+++ c-language/string_connect_while.c
@@ -1,30 +1,39 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#define NULL '\0'
+
+void mystrcat(char a[], char b[]);
 
 int main(void)
 {
     char dst[40];
     char src[20];
-    int i = 0; 
-    int j;
 
     printf("문자열 배열 dst에 저장할 과일: ");
     scanf("%s", dst);
     printf("문자열 배열 src에 저장할 과일: ");
     scanf("%s", src);
 
-    while (dst[i] != '\0')
-    {
-        i++;
-    }
-
-    for (j = 0; src[j] != '\0'; j++)
-    {
-        dst[i + j] = src[j];
-    }
-    dst[i + j] = '\0';
+    mystrcat(dst, src);
 
     printf("문자열 연결 확인 출력: %s\n", dst);
 
     return 0;
+}
+
+void mystrcat(char a[], char b[])
+{
+    int i = 0;
+    int j;
+
+    while(a[i] != NULL)
+    {
+        i++;
+    }
+
+    for(j = 0; b[j] != NULL; j++)
+    {
+        a[i + j] = b[j];
+    }
+
+    a[i + j] = NULL;
 }
