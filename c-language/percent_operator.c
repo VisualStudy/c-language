a9ec016586d946d17a4a5f6b058e4553e4547ae4
지호 박 <sparksoldier@outlook.com>
2025-09-28 19:17:57 +0900

    percent operator

---
+++ c-language/percent_operator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 0;
+    int num2 = 0;
+
+    printf("두 수를 입력 받아 나머지 연산하기\ n");
+    printf("첫 번째 수: ");
+    scanf("%d", &num1);
+    printf("두 번쨰 수: ");
+    scanf("%d", &num2);
+    
+    int result = 0;
+    result = num1 % num2;
+
+    printf("두 수의 연산 결과: %d\n", result);
+ 
+    return 0;
+}
