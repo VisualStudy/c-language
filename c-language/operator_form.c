a17f0f3715af482bac9395bfb6496a820bf26c21
지호 박 <sparksoldier@outlook.com>
2025-09-29 22:40:13 +0900

    form and float

--- c-language/operator_form.c
+++ c-language/operator_form.c
@@ -6,7 +6,7 @@ int main(void)
     float j = 123.45678;
     printf("%d %x %o\n", i, i, i);
     printf("%05d %5d\n", i, i);
-    printf("%7.1f %f\n", j, j);
+    printf("%7.1f %f\n", j, j); // float는 실수를 완벽히 표현할 수 없다.
     
     return 0;
 }
