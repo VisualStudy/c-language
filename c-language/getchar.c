d86893a839fdfecf69532d2b7666cb5f542f78f2
VisualStudy <sparksoldier@outlook.com>
2025-10-09 23:15:16 +0900

    null

--- c-language/getchar.c
+++ c-language/getchar.c
@@ -3,10 +3,16 @@
 int main(void)
 {
     int ch;
+    char str[100] = "rabarataRaVaraTA";
     
     while ((ch = getchar()) != EOF)
     {
         putchar(ch);
+    }
+
+    while (str != '\0')
+    {
+
     }
     
     return 0;
