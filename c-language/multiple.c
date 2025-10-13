3f29f196629094000e8bee9ee95d252940cac1f6
지호 박 <sparksoldier@outlook.com>
2025-10-13 22:42:32 +0900

    int main

--- c-language/multiple.c
+++ c-language/multiple.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void main()
+int main(void)
 {
     int num1;
     int num2;
@@ -8,4 +8,6 @@ void main()
     num2 = 10;
     printf
     ("%d\n", num1*num2); // 프린트 함수 안에 세미콜론 포함 시 오류
+
+    return 0;
 }
