7bee7bcdca377060c8fcf7eaa9c53add9017eaa0
지호 박 <sparksoldier@outlook.com>
2025-10-17 23:58:50 +0900

    refactoring and plus

--- c-language/float_percent_zero.c
+++ c-language/float_percent_zero.c
@@ -3,7 +3,12 @@
 int main(void)
 {
     float a = 123.45;
-    printf("%08.3f", a); // 소수점도 자릿수 포함
+    printf("%08.3f\n", a); // 소수점도 자릿수 포함
+    float b;
+    int c;
+    c = a;
+    b = c;
+    printf("%6.2f", b);
 
     return 0;
 }
