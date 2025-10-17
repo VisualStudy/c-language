f5a10ef8c612256424c45726d9c1e4a9b03f1f74
지호 박 <sparksoldier@outlook.com>
2025-10-18 00:05:52 +0900

    int float

---
+++ c-language/int_float_extenstion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 111.22;
+    float b = 300;
+
+    printf("a의 값 = = > %d\n", a);
+    printf("b의 값 = = > %f\n", b); // %f 기본 10자리
+
+    return 0;
+}
