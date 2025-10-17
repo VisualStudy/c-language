58727f98ca6f9c597707f7cbdd520105c872808a
지호 박 <sparksoldier@outlook.com>
2025-10-17 17:07:24 +0900

    refactoring

--- c-language/printf_speci.c
+++ c-language/printf_speci.c
@@ -3,7 +3,7 @@
 int main(void)
 {
     printf("%d %d %d %d\n", 100, 200, 300);
-    printf("%d %d\n", 100, 200 , 300);
+    printf("%d %d\n", 100, 200, 300);
     printf("%d\n", 100, 200, 300);
 
     return 0;
