dcab6693e7b946186fc0328453d84ae0e2afe077
지호 박 <sparksoldier@outlook.com>
2025-10-30 23:50:18 +0900

    array address

---
+++ c-language/array_point_address.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char x[5] = {"yaho"};
+    char str[3][20] = {
+        "lets",
+        "go",
+        "together"
+    };
+
+    printf("%s\n", x);
+    printf("%s\n", &*(x + 0));
+    printf("%c\n", x[0]);
+    printf("%c\n", *(x + 0));
+    
+    printf("%c\n", x[1]);
+    printf("%c\n", *(x + 1));
+   
+
+    printf("%s\n", str[0]);
+    printf("%s\n", &*(str[0] + 0));
+    printf("%s\n", str[1]);
+    printf("%s\n", &*(str[1] + 0));
+    
+    printf("%c\n", str[0][1]);
+    printf("%c\n", *(str[0] + 1));
+
+    return 0;
+}
