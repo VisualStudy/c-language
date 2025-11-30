34621e896fb4b7ba587fb6b79a0fdfcbd0660d6f
지호 박 <sparksoldier@outlook.com>
2025-11-30 21:03:38 +0900

    CharArrayDemo

---
+++ c-language/CharArrayDemo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char s[7] = "CODE";
+    putchar(s[0]);
+    putchar(s[1]);
+    putchar(s[2]);
+    putchar(s[3]);
+    putchar('\n');
+    return 0;
+}
