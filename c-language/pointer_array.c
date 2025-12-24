5378a2a28d75c294cefd59b7b70ad75762434c27
VisualStudy <sparksoldier@outlook.com>
2025-12-24 22:59:48 +0900

    printf_s

--- c-language/pointer_array.c
+++ c-language/pointer_array.c
@@ -7,10 +7,10 @@ int main(int argc, char *argv[])
     int arr[] = {10, 20, 30, 40, 50};
     int *p = arr;
 
-    printf("%s\n", TITLE);
-    printf("%d\n", arr[1]);
-    printf("%d\n", p[1]);
-    printf("%d\n", *(p + 1));
+    printf_s("%s\n", TITLE);
+    printf_s("%d\n", arr[1]);
+    printf_s("%d\n", p[1]);
+    printf_s("%d\n", *(p + 1));
     // CTRL + SHIFT + 방향키: 자기 자신 탐색
 
     return EXIT_SUCCESS;
