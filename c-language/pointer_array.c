a7b742a0789cfa9929c53e54ffb383d278d07218
VisualStudy <sparksoldier@outlook.com>
2025-12-24 22:54:08 +0900

    .

--- c-language/pointer_array.c
+++ c-language/pointer_array.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     int arr[] = {10, 20, 30, 40, 50};
     int *p = arr;
 
-    printf("pointer 이해하기\n");
+    printf("UnderstandingPointer\n");
     printf("%d\n", arr[1]);
     printf("%d\n", p[1]);
     printf("%d\n", *(p + 1));
