50fac33d0bf19484e3f8a224e85028d7b33c029a
VisualStudy <sparksoldier@outlook.com>
2025-12-24 22:56:42 +0900

    title

--- c-language/pointer_array.c
+++ c-language/pointer_array.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TITLE "Understanding Pointer"
 
 int main(int argc, char *argv[])
 {
     int arr[] = {10, 20, 30, 40, 50};
     int *p = arr;
 
-    printf("Understanding Pointer\n");
+    printf("%s\n", TITLE);
     printf("%d\n", arr[1]);
     printf("%d\n", p[1]);
     printf("%d\n", *(p + 1));
