544c7ad53e0f85c02a4d7f0413f64575f46690af
VisualStudy <sparksoldier@outlook.com>
2026-03-07 22:48:14 +0900

    refactoring

--- SCNU/fibonacci_series.c
+++ SCNU/fibonacci_series.c
@@ -2,8 +2,15 @@
 
 int main(void)
 {
-	int number1 = 1;
-	int number2 = 1;
+	printf("피보나치 수열 구현하기\n");
+	printf("초기값: 1\n");
 	
+	int num1 = 1;
+	int num2 = 1;
+	int num3;
+	num3 = num1 + num2;
+
+	printf("%d + %d = %d\n", num1, num1, num3);
+
 	return 0;
 }
